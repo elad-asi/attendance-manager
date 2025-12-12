8a0d5752f01314b6886ef449e4b6561c0fe75196
Asi Elad <asi@monogoto.io>
2025-12-12 14:10:12 +0200

    v0.6.5 - Force redeploy

--- frontend/app.js
+++ frontend/app.js
@@ -3,7 +3,7 @@
 // ============================================
 
 // Version
-const FE_VERSION = '0.6.4';
+const FE_VERSION = '0.6.5';
 
 // Auto-polling configuration
 const POLL_INTERVAL_MS = 10000; // 10 seconds
